5eed9f162a636a5610af9880ecbfe2cc59b7acc1
amar <amarvadla27@gmail.com>
2019-01-15 16:54:43 +0530

    added GET /todo/:id

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {Todo} = require('./models/todo');
-
+var {ObjectId} = require('mongodb').ObjectId;
 var app = new express();
 
 app.use(bodyParser.json());
@@ -33,6 +33,26 @@ app.get('/todo',(req,res)=>{
   });
 
 });
+
+app.get('/todo/:id',(req,res)=>{
+
+  var id = req.params.id;
+
+  if(!ObjectId.isValid(id)){
+    return res.status(404).send("unable to fetch id");
+  }
+
+  Todo.findById(id).then((data)=>{
+      if(!data){
+        return res.status(404).send("can not fetch the id");
+      }
+      res.send({data});
+  }).catch((e)=>{
+    res.status(404).send("error occured")
+  });
+
+});
+
 
 app.listen(3000,()=>{
   console.log('started on 3000');
